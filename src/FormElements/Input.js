78696d22b938001abc41c341baa3f56f187c24f1
QasimTariq <qtariq456@gmail.com>
2023-01-04 02:32:02 +0500

    Subject: Code Bug Predictor

---
+++ src/FormElements/Input.js
@@ -0,0 +1,87 @@
+import React, { useReducer, useEffect } from "react";
+
+import { validate } from "../util/validators";
+
+import "./Input.css";
+
+const InputReducer = (state, action) => {
+  switch (action.type) {
+    case 'CHANGE':
+      return {
+        ...state,
+        value: action.val,
+        isValid: validate(action.val, action.validation),
+      };
+    case 'TOUCH': {
+      return {
+        ...state,
+        isTouch: true,
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+const Input = (props) => {
+  const [inputState, dispatch] = useReducer(InputReducer, {
+    value: props.value || '',
+    isValid: props.valid || false,
+    isTouch: false,
+  });
+
+  const { id, onInput } = props;
+  const { value, isValid } = inputState;
+
+  useEffect(() => {
+    onInput(id, value, isValid)
+  }, [id, value, isValid, onInput]);
+
+  const changeHandler = (event) => {
+    dispatch({
+      val: event.target.value,
+      type: 'CHANGE',
+      validation: props.validators,
+    });
+  };
+
+  const touchHandler = () => {
+    dispatch({
+      type: 'TOUCH',
+    });
+  };
+
+  const element =
+    props.element === 'input' ? (
+      <input
+        id={props.id}
+        type={props.type}
+        placeholder={props.placeholder}
+        onChange={changeHandler}
+        value={inputState.value}
+        onBlur={touchHandler}
+      />
+    ) : (
+      <textarea
+        onChange={changeHandler}
+        value={inputState.value}
+        id={props.id}
+        rows={props.rows || 3}
+        onBlur={touchHandler}
+      />
+    );
+
+  return (
+    <div
+      className={`form-control ${
+        !inputState.isValid && inputState.isTouch && 'form-control--invalid'
+      }`}
+    >
+      <label htmlFor={props.id}>{props.label} </label>
+      {element}
+      {!inputState.isValid && inputState.isTouch && <p>{props.errorText}</p>}
+    </div>
+  );
+};
+
+export default Input;
